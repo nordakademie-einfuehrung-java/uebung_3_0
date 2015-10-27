1c4a122720578d246ae0f7f37f77165770d06bbb
14126 <14126@A001-16.nordakademie.intern>
2015-10-27 10:35:37 +0100

    Loesung 3_0 J.Moerker & E. Kapitza

---
+++ src/uebung_3_0/Rechenoperationen.java
@@ -0,0 +1,30 @@
+package uebung_3_0;
+
+public class Rechenoperationen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long i1 = 1000000;
+		long i2 = 1000000;
+		double i3 = 15;
+		byte i4 = 4;
+		double d1 = 1E200;
+		double d2 = 1E200;
+		double d3 = 0.1;
+		double d4 = 0.1;
+		
+	
+		double loesung1 = i1*i2;
+		double loesung2 = i3/i4;
+		double loesung3 = d1*d2;
+		double loesung4 = d3*d4;
+		
+
+	 System.out.println("Loesung 1 =" + loesung1);
+	 System.out.println("Loesung 2 =" + loesung2);
+	 System.out.println("Loesung 3 =" + loesung3);
+	 System.out.println("Loesung 4 =" + loesung4);
+	 
+	}
+
+}
